80c440874ba5ff802c771fbd8b3d55f03de5ce71
Prit Patel <pritpatel7311@gmail.com>
2024-11-04 12:14:59 -0500

    First Commit to this branch: Adding newly two files

---
+++ script.js
@@ -0,0 +1,14 @@
+//create a variable to hold a string value
+let student =
+  "You have received this message because you have been chosen to open an important vault. Here is the secret combination: ";
+
+//Crate three varibles, each varibale hodling a resulting number of a arithmatic operation
+const num1 = 6 + 6 - 2;
+const num2 = (16 / 2) * 5;
+const num3 = 39 % 50;
+
+//give an alert onto the webpage
+alert(`${student} ${num1} - ${num2} - ${num3}.`);
+
+//print a statement into the console tab
+console.log(`${student} ${num1} - ${num2} - ${num3}.`);
